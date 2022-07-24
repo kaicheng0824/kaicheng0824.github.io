545db331cd78b28530bd44a61424dc1f98dd590a
Kai-Wen Cheng <kac003@ucsd.edu>
2022-07-24 15:56:41 -0700

    part 1 modify

---
+++ customdialog.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    alertBtn = document.getElementById('alert')
+    alertBtn.addEventListener('click',showAlert());
+})
+
+function showAlert() {
+    let temp = document.getElementById('template');
+
+}
