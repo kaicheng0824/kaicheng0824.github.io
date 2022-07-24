aec0119014b8e29d6b086c314840e90c8dcc2a74
Kai-Wen Cheng <kac003@ucsd.edu>
2022-07-24 15:06:54 -0700

    Part 1

--- script.js
+++
@@ -1,8 +0,0 @@
-const alertBtn = document.getElementById('alert');
-const confirm = document.getElementById('confirm');
-const prompt = document.getElementById('prompt');
-const safe_prompt = document.getElementById('safe_prompt');
-
-alertBtn.addEventListener('click', function() {
-    window.alert('Alert!');
-});
